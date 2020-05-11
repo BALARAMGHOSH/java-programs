fd72f0122903c6eb4376a1886fc696d43674033f
Balaram Ghosh <48158402+BALARAMGHOSH@users.noreply.github.com>
2020-05-11 21:03:43 +0530

    Add files via upload

---
+++ addTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addTwoNumbers{
+
+     public static void main(String []args){
+        Scanner ip=new Scanner(System.in);
+        System.out.printf("enter the 2 numbers:");
+        int c;
+        int a=ip.nextInt();
+        int b=ip.nextInt();
+        c=a+b;
+        System.out.println("the answer is "+c);
+     }
+}
